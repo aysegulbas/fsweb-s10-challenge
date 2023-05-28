0c835919e8e33c5b37f6d94f4c87133dfc4b2e06
Aysegul BAS <aysegul.bas@arkas.com>
2023-05-28 16:42:02 +0300

    tamam

--- src/reducers.js
+++ src/reducers.js
@@ -1,10 +1,12 @@
+import { NOT_EKLE, NOT_SIL } from "./actions";
+
 const s10chLocalStorageKey = "s10ch";
 
 const baslangicDegerleri = {
   notlar: [
     {
       id: "75g1IyB8JLehAr0Lr5v3p",
-      date: "Fri Feb 03 2023 09:40:27 GMT+0300 (GMT+03:00)",
+      date: "Fri May 25 2023 09:40:27 GMT+0300 (GMT+03:00)",
       body: "Bugün hava çok güzel!|En iyi arkadaşımın en iyi arkadaşı olduğumu öğrendim :)|Kedim iyileşti!",
     },
   ],
@@ -24,6 +26,32 @@ function baslangicNotlariniGetir(key) {
   if (eskiNotlar) {
     return localStorageStateOku(key);
   } else {
-    return baslangicDegerleri
+    return baslangicDegerleri;
+  }
+}
+
+export function reducer(
+  state = baslangicNotlariniGetir(s10chLocalStorageKey),
+  action
+) {
+  switch (action.type) {
+    case NOT_EKLE:
+      const notEkleState = {
+        ...state,
+        notlar: [action.payload, ...state.notlar],
+      };
+      localStorageStateYaz(s10chLocalStorageKey, notEkleState);
+      return notEkleState;
+    case NOT_SIL:
+      const notSilState = {
+        ...state,
+        notlar: state.notlar.filter((not) => not.id !== action.payload),
+      };
+      localStorageStateYaz(s10chLocalStorageKey, notSilState);
+
+      return notSilState;
+
+    default:
+      return state;
   }
 }
